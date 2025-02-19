c096d1025c1bc6202b2744a45d15624151c0d0c0
Christianlauschlosser <chsc0003@stud.kea.dk>
2025-02-19 22:44:50 +0100

    Tilføjet rabat ved single view og rettet sidste css

--- js/produkt.js
+++ js/produkt.js
@@ -8,14 +8,17 @@ const identity = new URLSearchParams(window.location.search).get("id");
 if (identity) {
   // Henter data fra API'et
   fetch(`https://dummyjson.com/products/${identity}`)
-    .then((response) => response.json()) // Konverterer svaret til JSON
+    .then((response) => response.json())
     .then((data) => showProduct(data)); // Sender data til funktion
 } else {
   console.error("Produkt ID mangler i URL'en.");
 }
 
-// Funktion der viser produktet på siden
+// Funktion der viser produktet i HTML
 function showProduct(data) {
+  // Beregn rabatteret pris
+  const discountedPrice = (data.price * (1 - data.discountPercentage / 100)).toFixed(2);
+
   let markup = `
     <div class="product_image_container">
         <img src="${data.thumbnail}" alt="${data.title}" class="product_image">
@@ -23,10 +26,25 @@ function showProduct(data) {
     <div class="product_info">
         <h2 class="product-brand">${data.brand || "Unknown Brand"}</h2>
         <p class="product-name">${data.title}</p>
-        <p class="product-price">DKK ${data.price},00</p>
+
+        <p class="product-price">
+          ${
+            data.discountPercentage > 0
+              ? `<span class="old-price">${data.price} EUR</span> 
+               <span class="new-price">${discountedPrice} EUR</span>`
+              : `${data.price} EUR`
+          }
+        </p>
+
         <p class="product-description">${data.description}</p>
-        <div class="product-rating">${"⭐".repeat(Math.round(data.rating)) + "☆".repeat(5 - Math.round(data.rating))}</div>
-        <button class="buy-button">Buy</button>
+        
+        <div class="product-rating">
+          ${"⭐".repeat(Math.round(data.rating)) + "☆".repeat(5 - Math.round(data.rating))}
+        </div>
+
+        
+
+        <button class="buy-button">Buy Now</button>
     </div>
   `;
 

6b0d6a836839dad9fc35198c04d0dc1b3e5d5cd3
jova0002 <jova0002@stud.kea.dk>
2025-02-20 09:18:42 +0100

    NewCategoryPics_Josie

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ let knapper = document.querySelector(".knapper");
 
 // Definer de kategorier, vi vil vise, og deres billeder
 const categories = [
-  { name: "all", displayName: "All Products", image: "Pics/Gallery-pic-4.webp" },
-  { name: "beauty", displayName: "Beauty", image: "Pics/Gallery-pic-2.webp" },
-  { name: "fragrances", displayName: "Fragrances", image: "Pics/Gallery-pic-5.webp" },
+  { name: "all", displayName: "All Products", image: "Pics/cover_pic_1.webp" },
+  { name: "beauty", displayName: "Beauty", image: "Pics/cover_pic_3.webp" },
+  { name: "fragrances", displayName: "Fragrances", image: "Pics/cover_pic_2.webp" },
 ];
 
 // Funktion til at vise kategorierne
